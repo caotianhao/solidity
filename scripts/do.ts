e74e8150fd4db8afa7cddacef4e51fcb9b9211b3
caotianhao <caotianhao@naver.com>
2025-08-31 13:09:27 +0800

    feat: do tx

---
+++ scripts/do.ts
@@ -0,0 +1,29 @@
+import {ethers} from "hardhat";
+
+async function main() {
+    const provider = ethers.provider; // Hardhat 自带 provider
+    const [sender, receiver] = await ethers.getSigners();
+
+    console.log("Sender:", sender.address);
+    console.log("Receiver:", receiver.address);
+
+    let senderBalance = await provider.getBalance(sender.address);
+    let receiverBalance = await provider.getBalance(receiver.address);
+    console.log("Sender balance:", ethers.formatEther(senderBalance));
+    console.log("Receiver balance:", ethers.formatEther(receiverBalance));
+
+    const tx = await sender.sendTransaction({
+        to: receiver.address,
+        value: ethers.parseEther("1.0"),
+    });
+    await tx.wait();
+
+    console.log("Transaction hash:", tx.hash);
+
+    senderBalance = await provider.getBalance(sender.address);
+    receiverBalance = await provider.getBalance(receiver.address);
+    console.log("Sender balance after:", ethers.formatEther(senderBalance));
+    console.log("Receiver balance after:", ethers.formatEther(receiverBalance));
+}
+
+main().catch(console.error);
